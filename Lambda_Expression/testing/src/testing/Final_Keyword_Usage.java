90e5d2771b4b0dfe8483807783d7e434d6bf6e95
Kishan <kishan.dhongadi@gmail.com>
2019-04-21 20:35:53 +0530

    Added Final Variable Comments

--- Lambda_Expression/testing/src/testing/Final_Keyword_Usage.java
+++ Lambda_Expression/testing/src/testing/Final_Keyword_Usage.java
@@ -7,6 +7,23 @@ public class Final_Keyword_Usage {
 		System.out.println(a);
 		
 		System.out.println("Hello");
+		
+		/*Usage of final
+		 * 1) Final variable cannot be changed.
+		 * 2) final method cannot be override.
+		 * 3) final method can be inherited.
+		 * 4) if final variable is not initialised then it is called "Blank Final Variable"
+		 * 5) exact usage of final is by using constructor
+		 * 	like example
+		 * final int PanCard;
+		 * it can be changed by constructor(int a....,int PanCard)
+		 * {
+		 * this.PanCard = PanCard;
+		 * ...
+		 * }
+		 * 
+		 */
+		
 	}
 
 }
