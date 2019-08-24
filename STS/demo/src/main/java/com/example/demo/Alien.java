2dda365fd1dae3f1efd8eb354cf4c85e3d244b10
kdhongad <kdhongad@KDHONGAD-LAP.in.oracle.com>
2019-08-24 18:55:44 +0530

    Spring Boot

---
+++ STS/demo/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Alien {
+	
+	private int id;
+	private String name;
+	private String Tech;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTech() {
+		return Tech;
+	}
+	public void setTech(String tech) {
+		Tech = tech;
+	}
+}
